4a6ca59e4fd90ba352f9bf8b3f872482bd3bd081
Stephen Hester <stephenhester87@gmail.com>
2026-01-17 17:28:15 -0500

    Update GradeTracker.py

--- 1 gradetracker/GradeTracker.py
+++ 1 gradetracker/GradeTracker.py
@@ -9,7 +9,7 @@ See https://www.geeksforgeeks.org/python-docstrings/
 import statistics as s
 
 #add constants next
-admins = {'Dominic':'Hacker','Faculty2':'ABC123','Faculty3':'123ABC'}
+admins = {'Dominic':'Hacker','Faculty2':'ABC123','Faculty3':'123ABC','Alex':'Hester'}
 
 # Like the admins above is a dictionary but of students.
 # Dictionaries use curly brackets with colons to associate keys with values.
